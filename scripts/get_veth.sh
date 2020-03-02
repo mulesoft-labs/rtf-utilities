6d89b7caa6e138c82d2a15ada89f133d9ac5fe28
yuxiaojian <steven.yu@mulesoft.com>
2020-03-02 16:18:58 +1100

    remove -e

--- scripts/get_veth.sh
+++ scripts/get_veth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -o pipefail
 
 if [ -z "$1" ]; then
     echo "Container ID must be specified"
@@ -7,5 +7,5 @@ fi
 
 GRAVITY_BASH="gravity planet enter -- --notty /usr/bin/bash -- -c"
 INDEX=$($GRAVITY_BASH "docker exec $1 /bin/bash -c 'cat /sys/class/net/eth0/iflink'")
-VETH=`ip link | grep ^$INDEX | cut -d ":" -f2 | cut -d "@" -f1`
+VETH=$(ip link | grep ^$INDEX | cut -d ":" -f2 | cut -d "@" -f1)
 echo ${VETH//[[:blank:]]/}
