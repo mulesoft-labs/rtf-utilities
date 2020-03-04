e5fd297c0c60978db5bb1ed31a095a033c32eae1
yuxiaojian <steven.yu@mulesoft.com>
2020-03-04 14:43:14 +1100

    revoke changes

--- scripts/get_veth.sh
+++ scripts/get_veth.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-set -o pipefail
-set +e
+set -eo pipefail
 
 if [ -z "$1" ]; then
     echo "Container ID must be specified"
 fi
 
-#GRAVITY_BASH="gravity planet enter -- --notty /usr/bin/bash -- -c"
-gravity enter
-INDEX=$(docker exec $1 /bin/bash -c 'cat /sys/class/net/eth0/iflink')
+GRAVITY_BASH="gravity planet enter -- --notty /usr/bin/bash -- -c"
+INDEX=$($GRAVITY_BASH "docker exec $1 /bin/bash -c 'cat /sys/class/net/eth0/iflink'")
 VETH=$(ip link | grep ^$INDEX | cut -d ":" -f2 | cut -d "@" -f1)
 
 echo ${VETH//[[:blank:]]/}
